711076078bfda12bc4ef660636d970e905336f34
samu <samsoad@hotmail.com>
2023-10-11 18:50:07 -0300

    seccion 3 y 4, falta mejorar

--- src/Components/sections/sectHome/sect2/index.jsx
+++ src/Components/sections/sectHome/sect2/index.jsx
@@ -15,18 +15,21 @@ return (
 
 
 <div className="bl1">
-   <p > 1<br /> Contactános a través del <br />formulario, deja tus datos y <br />describí brevemente la situación.<br /> También podes contactarnos a <br /> través del whatsapp</p>
+<div className="bl1tit">1</div>
+   <p >Contactános a través del <br />formulario, deja tus datos y <br />describí brevemente la situación.<br /> También podes contactarnos a <br /> través del whatsapp</p>
    </div>    
 
 
 
    <div className="bl2">
-   <p> 2<br />Quedáte atento al correo que de <br />inmediato de responderemos con<br /> un numero de ticket y los días <br /> disponibles para una visita, si es<br /> necesaria. Te indicaremos las <br />formas de pago para agendar la <br />visita.</p>
+   <div className="bl2tit">2</div>
+   <p>Quedáte atento al correo que de <br />inmediato de responderemos con<br /> un numero de ticket y los días <br /> disponibles para una visita, si es<br /> necesaria. Te indicaremos las <br />formas de pago para agendar la <br />visita.</p>
    </div>    
 
 
    <div className="bl3">
-   <p> 3<br />Una vez recibida tu confirmación,<br /> recibirás un correo con los datos <br />del técnico que te visitará.<br /> Si decidís traer el equipo, te <br />aseguramos el diagnóstico el<br /> mismo día!</p>
+   <div className="bl3tit">3</div>
+   <p>Una vez recibida tu confirmación,<br /> recibirás un correo con los datos <br />del técnico que te visitará.<br /> Si decidís traer el equipo, te <br />aseguramos el diagnóstico el<br /> mismo día!</p>
    </div>      
 
      
