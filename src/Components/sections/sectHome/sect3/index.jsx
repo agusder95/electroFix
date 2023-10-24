4ff4ec67d6b36a6fad940e6ef6d632dad72e8353
Agus <agustin.derobles1995@gmail.com>
2023-10-24 18:09:51 -0300

    new design

--- src/Components/sections/sectHome/sect3/index.jsx
+++ src/Components/sections/sectHome/sect3/index.jsx
@@ -1,10 +1,9 @@
 import "./styles.scss";
-
-import Title from "../../../common/titleBar";
 import img1 from "../../../../assets/images/home/Vector-1.png";
 import img2 from "../../../../assets/images/home/Vector-2.png";
 import img3 from "../../../../assets/images/home/Vector-3.png";
 import img4 from "../../../../assets/images/home/Vector-4.png";
+import Card from "./card/card";
 
 const Sect3 = () => {
   /*
@@ -18,38 +17,13 @@ const Sect3 = () => {
 
   return (
     <section className="section3">
-      <div className="block">
-        <Title text={"Por qué electroFix ?"} />
+      <h2 id="title">Por qué electroFix?</h2>
 
-        <div className="bl1">
-          <img src={img1} alt="" />
-
-          <div className="bl1tit">Garantía</div>
-
-          <p>Garantizamos nuestros trabajos 100%</p>
-        </div>
-
-        <div className="bl2">
-          <img src={img2} alt="" />
-
-          <div className="bl2tit">Confiabilidad</div>
-
-          <p>Nos caracteriza la seriedad y la puntualidad</p>
-        </div>
-
-        <div className="bl3">
-          <img src={img3} alt="" />
-
-          <div className="bl3tit">Eficiencia</div>
-          <p>Agilizamos tu trabajo y anticipamos las entregas</p>
-        </div>
-
-        <div className="bl4">
-          <img src={img4} alt="" />
-
-          <div className="bl4tit">Calidad</div>
-          <p>Estamos comprometidos con la calidad desde el primer contacto</p>
-        </div>
+      <div className="cardSection">
+        <Card className="cardSect3" imge={img1} title="Garantía" txt="Garantizamos nuestros trabajos 100%" />
+        <Card className="cardSect3" imge={img2} title="Confiabilidad" txt="Nos caracteriza la seriedad y la puntualidad" />
+        <Card className="cardSect3" imge={img3} title="Eficiencia" txt="Agilizamos tu trabajo y anticipamos las entregas" />
+        <Card className="cardSect3" imge={img4} title="Calidad" txt="Estamos comprometidos con la calidad desde el primer contacto" />
       </div>
     </section>
   );
--- src/Components/sections/sectHome/sect4/index.jsx
+++ src/Components/sections/sectHome/sect4/index.jsx
@@ -1,14 +1,13 @@
 import "./styles.scss";
 import Title from "../../../common/titleBar";
 import img from "../../../../assets/images/home/map-icon.png";
-
+import Image from "../../../common/Image";
 const Sec4 = () => {
   return (
     <section className="section4">
-      <div className="block">
-        <Title text={"Cobertura de nuestros servicios"} />
-
-        <img src={img} alt="" />
+      <h2>Cobertura de nuestros servicios</h2>
+      <div className="imageSect4Container">
+        <Image src={img} alt="" />
       </div>
     </section>
   );
